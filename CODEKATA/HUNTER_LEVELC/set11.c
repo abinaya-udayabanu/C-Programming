c5987de20e32e5b47b7d2a73b353773bdc4ef19c
abinaya-udayabanu <34646478+abinaya-udayabanu@users.noreply.github.com>
2018-05-19 18:54:29 +0530

    Create set11.c

---
+++ CODEKATA/HUNTER_LEVELC/set11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	long int a[10000];
+	long int i,j,n;
+	long int temp;
+	scanf("%ld",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%ld",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+		if(a[i]<=a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==a[i+1])
+		{
+			
+			while(i<n)
+			{
+				if(a[i]!=a[i+1])
+				{
+					printf("%ld ",a[i]);
+					break;
+				}
+				i++;
+			}
+		}
+	}
+	return 0;
+}
